1511736e4d0865a59955c33a2b9e99c239b369f4
Ighere2022 <Uyoyo2003@gmail>
2024-06-26 13:27:16 +0100

    update the production url

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 const devBaseUrl = "http://localhost:5000";
 const productionUrl = "https://lab-phase.onrender.com";
 const api = axios.create({
-  baseURL: `${devBaseUrl}/api`,
+  baseURL: `${productionUrl}/api`,
 });
 export default api;
